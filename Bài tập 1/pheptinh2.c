5c921d2de79b6915691bba437facc42990f8f2b3
Mai Trung Hieu <72543235+MaiTrungHieu249@users.noreply.github.com>
2020-11-26 16:11:27 +0700

    Update pheptinh2.c

--- Bài tập 1/pheptinh2.c
+++ Bài tập 1/pheptinh2.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
 
 int main (void){
-	
+	//khai bao bien a,b va gan a=3, b=5
 	int a ;int b;
 	a=3;b=5;
 	
 	a+b; a-b; a*b; a/b;
 	
-	double c = 3;
-    double d = 5;
+	//khai bao bien thuong
+	double thuong;
 	
+	//ep kieu 
+	thuong =(double) a/b;
+	
+	//in ra man hinh 
 	printf("a\t b\t a+b\t a-b\t a*b\t a/b\n");
 	
-	printf("%d\t %d\t %d\t %d\t %d\t %g",a,b,a+b,a-b,a*b,c/d);
+	//in ket qua ra man hinh
+	printf("%d\t %d\t %d\t %d\t %d\t %g",a,b,a+b,a-b,a*b,thuong);
 
     
 	return 0;
