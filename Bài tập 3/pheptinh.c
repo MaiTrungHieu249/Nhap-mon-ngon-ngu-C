65acabcffcb924c964ab8d93f8ea7ea61b05fc35
Mai Trung Hieu <72543235+MaiTrungHieu249@users.noreply.github.com>
2020-12-17 16:01:46 +0700

    Update pheptinh.c

--- Bài tập 3/pheptinh.c
+++ Bài tập 3/pheptinh.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 int main(){
-  int a = 5, b = 5;
+  int a = 2, b = 8;
   char pt;
   printf ("vui long nhap phep tinh ");
   scanf ("%c",&pt);
   switch(pt) {
-     case '+': printf (" a + b = %d",a +b);
+     case '+': 
+      printf (" a + b = %d",a +b);
       break;
-  	 case '-': printf (" a - b = %d",a -b);
+  	 case '-': 
+      printf (" a - b = %d",a -b);
       break;
       default:
        printf(" nhap cong hoac tru ");
