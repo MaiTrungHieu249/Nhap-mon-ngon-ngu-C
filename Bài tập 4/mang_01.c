c52513574f19b4b9d3040645b20e5374710f069f
Mai Trung Hieu <72543235+MaiTrungHieu249@users.noreply.github.com>
2021-01-14 15:52:26 +0700

    Create mang_01.c

---
+++ Bài tập 4/mang_01.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(void){
+	
+    int a[4];// khai bao hop 4 ngan
+    a[0] = 3;   // ngan thu 1
+    a[1] = 5;   // ngan thu 2
+	a[2] = 7;   // ngan thu 3 
+    a[3] = 2;   // ngan thu 4
+    
+    //tinh tong cac phan tu mang a  
+   int sum = 0,i=0;
+ for(; i < 4; i++){
+   sum=sum+a[i];
+}
+    printf("%d",sum);
+ 
+     return 0;
+}
