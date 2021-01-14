e02a5dcbde18c65ed1411acc4a4ae856054bd09f
Mai Trung Hieu <72543235+MaiTrungHieu249@users.noreply.github.com>
2021-01-14 16:10:51 +0700

    Update mang_01.c

--- Bài tập 4/mang_01.c
+++ Bài tập 4/mang_01.c
@@ -5,7 +5,7 @@ int main(void){
     int a[4];// khai bao hop 4 ngan
     a[0] = 3;   // ngan thu 1
     a[1] = 5;   // ngan thu 2
-	a[2] = 7;   // ngan thu 3 
+    a[2] = 7;   // ngan thu 3 
     a[3] = 2;   // ngan thu 4
     
     //tinh tong cac phan tu mang a  
