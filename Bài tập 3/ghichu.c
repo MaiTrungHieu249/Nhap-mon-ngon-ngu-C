1eddb3f606383fa2eccca74ab2bde256dc796ce9
Mai Trung Hieu <72543235+MaiTrungHieu249@users.noreply.github.com>
2021-01-14 15:29:03 +0700

    Update and rename chuthich.c to ghichu.c

---
+++ Bài tập 3/ghichu.c
@@ -0,0 +1,36 @@
+break kết thúc 1 lệnh
+	 switch thay if trong 1 số trường hợp số hoặc kí tự
+	 default các trường hợp còn lại
+	  case các trường hợp
+	  #cú pháp
+	  switch
+	  case + trường hợp
+	  break để kết thúc lệnh
+	  printf để in
+	  default các trường hợp còn lại để kết thúc
+
+  for(khoi tao bo dem;dk;buoc dem){
+	  các lenh}
+	  
+	  
+	 khoi tao bo dem
+	while(dk){
+	lenh thuc thi
+	buoc dem
+	}
+
+---------------------------------------------------------------------------------------------------------------
+    int a;// khai bao hop 1 ngan
+    a = 3;
+    
+    int b[3];// khai bao hop 3 ngan
+    b[0] = 3;   // ngan thu 1
+    b[1] = 4;   // ngan thu 2
+	b[2] = 1;   // ngan thu 3 
+    
+    float c[4] = {1.5,2.3,0.5,2.7} ; // khai bao 1 hop 4 ngan chua so kieu float 
+     
+    // hien thi tat ca cac phan tu cua mang b 
+    for(int i = 0; i <= 3; i++);
+    printf("%d",b[i]);
+  
