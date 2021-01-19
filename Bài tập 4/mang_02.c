a16fdb1af51d3a2c693ff17fb485c39c1f03a9ee
Mai Trung Hieu <72543235+MaiTrungHieu249@users.noreply.github.com>
2021-01-19 16:24:33 +0700

    Update mang_02.c

--- Bài tập 4/mang_02.c
+++ Bài tập 4/mang_02.c
@@ -4,17 +4,18 @@ int main(void) {
   int n;
   printf("nhap n= ");
   scanf("%d",&n);
+  
   int a[n];
-
   for(int i=0;i < n ;i++){
     printf("nhap phan tu thu %d: ",i+1);
         scanf("%d", &a[i]);
   }
-   printf("mang a: ");
+    printf("mang a: ");
   for(int i=0; i < n; i++){
-        printf(" %d",a[i]);
-      }
-      printf("\nso chan tu mang a: ");
+    printf(" %d",a[i]);
+  }
+    
+	printf("\nso chan tu mang a: ");
   for(int i=0;i<n;i++){
         if(a[i] % 2==0){
 		printf("%d ",a[i]);
