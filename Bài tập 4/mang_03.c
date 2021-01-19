72f612e2fe95ab453ab56dd314cfbe3460450fc7
Mai Trung Hieu <72543235+MaiTrungHieu249@users.noreply.github.com>
2021-01-19 16:10:42 +0700

    Create mang_03.c

---
+++ Bài tập 4/mang_03.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main(void) {
+  //khai bao va nhap n 
+  int n,sc=0,sl=0,ko=0;
+  printf("nhap n= ");
+  scanf("%d",&n);
+  int a[n];
+
+  for(int i=0;i < n ;i++){
+    printf("nhap phan tu thu %d: ",i+1);
+        scanf("%d", &a[i]);
+  }
+  //hien thi cac phan tu cua mang a ra man hinh
+    printf("mang a: ");
+  for(int i=0; i < n; i++){
+      printf(" %d",a[i]);
+      }
+      //hien thi ra man hinh so chan trong mang a
+  printf("\nso chan tu mang a: ");
+
+  for(int i=0;i<n;i++){
+        if(a[i] % 2==0 &&a[i]>0){ // lay cac so chan trong mang a 
+        printf("%d ",a[i]);
+        sc+=1;
+        }
+  }
+        printf("\nso le tu mang a: ");
+  for(int i=0;i<n;i++){
+        if(a[i] % 2!=0){
+        printf("%d ",a[i]);
+        sl+=1;
+    }
+    }    
+  for(int i=0;i<n;i++){
+        if(a[i] ==0){
+        ko+=1;
+    }
+  }
+  printf("\nso cac so chan trong mang a: %d ",sc);
+  printf("\nso cac so le trong mang a: %d ",sl);
+  printf("\nso luong so 0: %d ",ko);
+  return 0;
+}
