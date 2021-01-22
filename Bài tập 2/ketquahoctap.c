ae6988c422d179df5b80470594002a40b8b30c41
Mai Trung Hieu <72543235+MaiTrungHieu249@users.noreply.github.com>
2021-01-22 09:40:29 +0700

    Create ketquahoctap.c

---
+++ Bài tập 2/ketquahoctap.c
@@ -0,0 +1,21 @@
+  
+#include <stdio.h>
+
+int main(void) {
+  // khai bao bien Van(V) , Toan(T) va nhap du lieu tu ban phim
+  double V,T;
+  //khai bao bien Diem Trung Binh(DTB)
+  double DTB;
+  DTB=(V + T)/2;
+  printf("Nhap Diem Van V = ");//nhap tu ban phim
+  scanf("%lf",&V);
+  printf("Nhap Diem Toan T = ");//nhap tu ban phim
+  scanf("%lf",&T);
+  //Hien thi tieu de
+  printf("Diem Van(V)\t Diem Toan(T)\t Diem Trung Binh(DTB)\t \n");
+  //Hien thi ket qua
+  printf("%.2f\t         %.2f\t          %.2f\t \n",V, T, (V + T)/2);
+
+  getchar();
+  return 0;
+}
