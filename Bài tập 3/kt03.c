b49045b346bd746065c0e83b367eb6710451364b
Mai Trung Hieu <72543235+MaiTrungHieu249@users.noreply.github.com>
2021-01-22 09:44:58 +0700

    Create kt03.c

---
+++ Bài tập 3/kt03.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+int main(void){
+  for(int i = 1; i <= 4; i++){
+    if(i % 2 != 0 ){
+      printf(" %d", i);
+    }
+  }
+  for(int i = 6; i <= 10; i++){
+    if(i % 2 != 0){
+      printf(" %d", i);
+    }
+  }
+  for(int i = 12; i <= 20; i++ ){
+    if(i % 2 != 0){
+      printf(" %d", i);
+    }
+  }  
+  return 0;
+}  
+
+vòng lặp for cách 2
+  
+#include<stdio.h>
+
+int main(void){
+
+  for(int i = 1; i <= 20; i+=2){
+    if((i == 5) || ( i == 11) ||( i == 15)) 
+      continue;
+     
+      
+      printf(" %d", i);
+    
+    }
+  return 0;
+}  
