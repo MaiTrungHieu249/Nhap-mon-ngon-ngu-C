9ad7de84391ab4e2256c2fa33ee1c3718a53c315
Mai Trung Hieu <72543235+MaiTrungHieu249@users.noreply.github.com>
2021-01-23 09:56:01 +0700

    Update TH_02.c

--- Bài tập 5/TH_02.c
+++ Bài tập 5/TH_02.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
  int main(){
    int n, tong = 0;
+   
    printf ("nhap so nguyen bat ky n = ");
    scanf ("%d", &n);
-   for (int i = 0; i < n; i++){
-       tong = tong + i+1;
+   
+   for (int i = 1; i <= n; i++){
+       tong = tong + i;
    }
-     printf (" tong can tim %d ", tong);
+   printf("S = 1 + 2 + ... + %d",n);
+     printf (" \ntong can tim %d ", tong);
+     
+     
    return 0;
    }
