53a7b4dce190d6bac68e33ccdd5cf08e63283ed7
Mai Trung Hieu <72543235+MaiTrungHieu249@users.noreply.github.com>
2021-01-23 10:54:51 +0700

    Update kt07.c

--- Bài tập 3/kt07.c
+++ Bài tập 3/kt07.c
@@ -1,12 +1,16 @@
 int main(void) {
+  
 // khoi tao bien
+  
   int a, b, c;
+  
   printf("nhap canh a :");
   scanf("%d", &a);
   printf("nhap canh b :");
   scanf("%d", &b);
   printf("nhap canh c :");
   scanf("%d", &c);
+  
   if(a+b > c || a+c > b || b+c > a){
     printf("%d, %d, %d la ba canh cua tam giac", a, b, c);
   }else{
