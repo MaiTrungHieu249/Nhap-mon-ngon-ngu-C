6bedfbf0069656274a194d24a4c8d0e4b808587c
Mai Trung Hieu <72543235+MaiTrungHieu249@users.noreply.github.com>
2021-01-23 10:52:53 +0700

    Create TH_01.c

---
+++ Bài tập 5/TH_01.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+	float a, b, c,Denta;
+	
+	printf ("\n nhap vao a = ");
+	scanf ("%f", &a);
+	printf ("\n nhap vao b = ");
+	scanf ("%f", &b);
+	printf ("\n nhap vao c = ");
+	scanf ("%f", &c);
